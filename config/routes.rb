201e86c5c740bcd266b0f5d23fee0120b120f513
barnabtz <barnabtz@gmail.com>
2020-06-13 02:27:28 +0100

    setup new and create article route, view controller and error messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 end
