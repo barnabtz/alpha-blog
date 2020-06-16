9a270a11fe630e900e3faecb7ecca4852122ddd7
barnabtz <barnabtz@gmail.com>
2020-06-16 17:09:22 +0100

    scaffold home page front end with bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//Importing bootstrap, jquery and popper for frontend
+import "bootstrap"
+// import "popper.js"
+// import "jquery"
